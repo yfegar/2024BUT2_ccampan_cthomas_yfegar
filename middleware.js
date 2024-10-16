f0432f2ca68c53aa02937e2b7f06681ca4b58200
yfegar <youna.fegar@gmail.com>
2024-10-16 15:14:05 +0200

    css + modif middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 app.use(function(req, res, next) {
     // code exécuté tout le temps
     if (req.session.userId) {
-        req.local.isAuth = true; //local accessible dans les vues
-        req.local.id = req.session.userID;
+        req.locals.isAuth = true; //local accessible dans les vues
+        req.locals.id = req.session.userID;
     } else {
         req.local.isAuth = false;
     }
