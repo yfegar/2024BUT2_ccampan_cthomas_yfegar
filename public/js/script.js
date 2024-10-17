9148dfa6149bed2e45f9b97ac92c2c42367c8aba
yfegar <youna.fegar@gmail.com>
2024-10-17 11:42:49 +0200

    changement de couleur nav

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ let nav = document.querySelectorAll(".nav_lien");
 
 let currentLocation = window.location.href
 
-if(currentLocation == 'http://localhost:3000/') {
+if(currentLocation == 'http://localhost:3000/' || currentLocation == 'http://localhost:3000/apropos' || currentLocation == 'http://localhost:3000/inscription' || currentLocation == 'http://localhost:3000/connexion' ) {
     titre.classList.toggle("white");
     
     for (let i = 0; i < nav.length; ++i) {
